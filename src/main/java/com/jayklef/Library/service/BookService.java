b31e176d00894ca85be1d13c6bfe68ade24c56ea
Jayklef <arhawhojerry@gmail.com>
2025-03-30 15:09:34 +0100

    BookRepo/service/remove methods

---
+++ src/main/java/com/jayklef/Library/service/BookService.java
@@ -0,0 +1,36 @@
+package com.jayklef.Library.service;
+
+import com.jayklef.Library.entity.Book;
+import com.jayklef.Library.repository.BookRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> findAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Book findBook(Long id){
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not Found"));
+        return book;
+    }
+
+    public void  createBook(Book book){
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id){
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not Found"));
+        bookRepository.deleteById(book.getId());
+    }
+}
