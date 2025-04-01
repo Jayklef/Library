43507713b7f23bcc0240c2fd1a3f3d2406337bae
Jayklef <arhawhojerry@gmail.com>
2025-04-01 19:29:55 +0100

    Publisher service Impl

--- src/main/java/com/jayklef/Library/service/PublisherService.java
+++ src/main/java/com/jayklef/Library/service/PublisherService.java
@@ -1,7 +1,35 @@
 package com.jayklef.Library.service;
 
+import com.jayklef.Library.entity.Publisher;
+import com.jayklef.Library.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PublisherService {
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    public Publisher createPublisher(Publisher publisher){
+        return publisherRepository.save(publisher);
+    }
+
+    public List<Publisher> getAllPublishers(){
+        return publisherRepository.findAll();
+    }
+
+    public Publisher getPublisher(Long id){
+        Publisher publisher = publisherRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Publisher not found"));
+        return publisher;
+    }
+
+    public void deletePublisher(Long id){
+        Publisher publisher = publisherRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Publisher not found"));
+        publisherRepository.deleteById(publisher.getId());
+    }
 }
