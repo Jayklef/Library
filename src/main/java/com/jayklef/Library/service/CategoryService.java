db15ff5ed1f180cb9b62e95e4662752753ea27cd
Jayklef <arhawhojerry@gmail.com>
2025-04-01 19:24:12 +0100

    category service Impl

--- src/main/java/com/jayklef/Library/service/CategoryService.java
+++ src/main/java/com/jayklef/Library/service/CategoryService.java
@@ -1,7 +1,36 @@
 package com.jayklef.Library.service;
 
+import com.jayklef.Library.entity.Category;
+import com.jayklef.Library.entity.Publisher;
+import com.jayklef.Library.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> findAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategory(Long id){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category with id: " + id + "not found"));
+        return category;
+    }
+
+    public void deleteCategory(Long id){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category with id: " + id + "not found"));
+        categoryRepository.deleteById(category.getId());
+    }
 }
